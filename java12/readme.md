88b77dcfc2e1c83685b3faf1074b3ec1612e6f91
Mohamed Ali AMDOUNI <mohamedali.amdouni@decathlon.com>
2024-07-21 14:21:25 +0200

    feat(java-12): add teeing and compact numbers

--- java12/readme.md
+++ java12/readme.md
@@ -2,7 +2,7 @@
 
 ## API
 
-### Indentation : String
+### Indentation :: String
 
 Adjusts the indentation of each line of this string based on the value of n, and normalizes line termination characters.
 
@@ -10,10 +10,21 @@ Adjusts the indentation of each line of this string based on the value of n, and
 String indent(int n)
 ```
 
-### Transform : String
+### Transform :: String
 
 This method allows the application of a function to this string.
     
 ```java
 <R> R transform(Function<? super String,? extends R> f)
 ```
+
+### Compact Number Format
+
+The compact number format is a new feature in Java 12. It is used to format a number in a compact form.
+
+### Teeing Collector :: Stream
+
+This method is used to perform a transformation on the elements of the stream and then combine the results.
+Previously, we can use only one collector with the stream api, but now we can use two collectors and combine them to have a single result.
+
+![teeing.png](images%2Fteeing.png)
---
+++ java12/src/main/java/org/example/java/tutorial/java12/UsageTeeingCollector.java
@@ -0,0 +1,44 @@
+package org.example.java.tutorial.java12;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class UsageTeeingCollector {
+
+    public static void main(String[] args) {
+
+        var ints = buildIntStream();
+        log.info("List : {}",
+                ints.collect(Collectors.toList())
+        );
+
+        ints = buildIntStream();
+        log.info("Summing : {}",
+                ints.collect(Collectors.summingInt(Integer::valueOf))
+        );
+
+        ints = buildIntStream();
+        log.info("Counting : {}",
+                ints.collect(Collectors.counting())
+        );
+
+        // Teeing Collector
+        // The teeing collector is a new feature in Java 12 that is used to collect the result of two collectors.
+        ints = buildIntStream();
+        long average = ints.collect(
+                Collectors.teeing(
+                        Collectors.summingInt(Integer::valueOf),
+                        Collectors.counting(),
+                        (sum, count) -> sum / count
+                )
+        );
+        log.info("Average : {}", average);
+    }
+
+    private static Stream<Integer> buildIntStream() {
+        return Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+    }
+
+}
